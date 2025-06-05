921dc8e439acaaf686a431012c8ff17e1ed385fb
Islom <karimov.islam5828859@gmail.com>
2025-06-05 22:58:55 +0900

    fix: modify advert section styles

--- libs/components/homepage/Advertisement.tsx
+++ libs/components/homepage/Advertisement.tsx
@@ -7,7 +7,27 @@ const Advertisement = () => {
 	const device = useDeviceDetect();
 
 	if (device == 'mobile') {
-		return <Stack className={'approach'}>"ADVERTISEMENT MOBILE"</Stack>;
+		return (
+			<Stack className={'mobile-approach'} flexDirection={'column'}>
+				<Stack className={'container'}>
+					<Stack className={'approach-box'}>
+						<Box className={'approach-title'}>Our Approach to fashion design</Box>
+						<Box className={'approach-text'}>
+							at elegant vogue , we blend creativity with craftsmanship to create fashion that transcends trends and
+							stands the test of time each design is meticulously crafted, ensuring the highest quelity exqulsite finish
+						</Box>
+					</Stack>
+					<Stack className={'approach-images'} flexDirection={'row'}>
+						<Link href={'/product'} className={'approach-image-upper'}>
+							<img src="/img/home/fash1.jpg" alt="" />
+						</Link>
+						<Link href={'/product'} className={'approach-image-lower'}>
+							<img src="/img/home/fash2.jpg" alt="" />
+						</Link>
+					</Stack>
+				</Stack>
+			</Stack>
+		);
 	} else {
 		return (
 			<Stack className={'approach'} flexDirection={'column'}>
--- libs/components/homepage/MobileTop.tsx
+++ libs/components/homepage/MobileTop.tsx
@@ -236,7 +236,7 @@ const MobileTop = () => {
 			<AppBar
 				position="fixed"
 				sx={{
-					backgroundColor: '#fff',
+					background: 'transparent',
 					boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
 					color: '#333',
 				}}
